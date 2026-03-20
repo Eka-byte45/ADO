1b724490c8b146422b6b22082777834b965892f7
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-03-21 01:35:14 +0800

    Connector begin

---
+++ ADO/Connector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+	internal class Connector
+	{
+		string connection_string;
+		SqlConnection connection;
+		public Connector(string connection_string)
+		{
+			Console.WriteLine(connection_string);//Выводим строку подключения к локальной базе данных на консоль
+			this.connection_string = connection_string;
+			connection = new SqlConnection(connection_string);
+		}
+
+		public void Select(string cmd)
+		{
+			connection.Open();//Открываем соединение с  базой данных
+			SqlCommand command = new SqlCommand(cmd, connection);//Создание комманды SQL-запроса, привязываем к открытому соединению
+			SqlDataReader reader = command.ExecuteReader();//Выполнение запроса и получение результирующего набора данных
+														   // Перебор и вывод имен колонок в первой строке вывода
+			for (int i = 0; i < reader.FieldCount; ++i)
+			{
+				Console.Write(reader.GetName(i) + "\t");//Печатаем имена колонок через табуляцию
+			}
+			Console.WriteLine();
+			// Читаем данные построчно из полученного набора результатов
+			while (reader.Read())//пока есть доступные записи
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
+				// Перебираем поля текущего ряда и печатаем значения колонок
+				for (int i = 0; i < reader.FieldCount; ++i)
+				{
+					Console.Write($"{reader[i]}\t\t"); // Значения колонок выводятся через двойную табуляцию
+				}
+				Console.WriteLine();
+			}
+			// Завершаем работу с набором данных и закрываем его
+			reader.Close();
+			connection.Close();
+		}
+		public object Scalar(string cmd)
+		{
+			object result = null;
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+			result = command.ExecuteScalar();//Выполнение скалярного запроса
+			connection.Close();
+			return result;
+		}
+	}
+}
