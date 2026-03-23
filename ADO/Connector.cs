8cf37fed390dfe1c3783d0f6340a774641b61813
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-03-23 23:24:46 +0800

    Home Work2 DONE

--- ADO/Connector.cs
+++ ADO/Connector.cs
@@ -61,16 +61,18 @@ namespace ADO
 			connection.Close();
 			return result;
 		}
+		//Переделываем метод GetMaxPrimaryKey, используя GetPrimaryKeyName
 		public int GetMaxPrimaryKey(string table)
 		{
-			string cmd = $"SELECT * FROM {table}";
-			SqlCommand command = new SqlCommand(cmd, connection);
-			connection.Open();
-			SqlDataReader reader = command.ExecuteReader();
-			string pk_name = reader.GetName(0);
-			reader.Close();
-			connection.Close() ;
-			return (int)Scalar($"SELECT MAX({pk_name}) FROM {table}");
+			string pk_column_name = GetPrimaryKeyName(table);//получаем имя столбца, который является первичным ключом
+			if (pk_column_name == null)
+			{
+				throw new Exception($"Первичный ключ для таблицы {table} не найден");
+			}
+			string cmd = $"SELECT MAX([{pk_column_name}]) FROM {table}";//формируем запрос на получение максимального значения из столбца, который мы нашли с помощью функции GetPrimaryKeyName
+			//[] - служат для экранирования имен, если они содержат пробелы или зарезервированные слова
+			object result = Scalar(new SqlCommand(cmd));
+			return result == null ? 0 : Convert.ToInt32(result);
 		}
 		public int GetNextPrimaryKey(string table)
 		{
@@ -96,6 +98,105 @@ namespace ADO
 			
 			connection.Close ();
 		}
-		
+		//Переписанный метод Scalar, он принимает готовую команду с параметрами
+		public object Scalar(SqlCommand command)
+		{
+			command.Connection = connection;
+			object result = null;
+			connection.Open();
+			result = command.ExecuteScalar();
+			connection.Close();
+			return result;
+		}
+		//Метод полчения названия первичного ключа
+		public string GetPrimaryKeyName(string table)
+		{
+			string pk_name = null;//строка для хранения названия первичного ключа
+
+			//данный запрос получает имя столбца, который является первичным ключом
+			//в системной таблице INFORMATION_SCHEMA.KEY_COLUMN_USAGE запрос ищет, где имя таблицы совпадает с переданным параметром
+			//а имя ограничения начинается с PK
+			string cmd = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME = @table AND CONSTRAINT_NAME LIKE 'PK_%'";
+			//создание объекта комманды для выполнения SQL-запроса
+			SqlCommand command = new SqlCommand (cmd);
+			//здесь добавляем параметр в команду,защита от SQL-инъекций
+			command.Parameters.AddWithValue("@table",table);
+			object result = this.Scalar(command);//с помощью функции Scalar выполняем команду
+			pk_name = result.ToString();
+			return pk_name;
+		}
+		//Метод, проверяющий существование записи в базе данных
+		public bool RecordExists(string table, Dictionary <string,object> columns)
+		{
+			string conditions = "";//строка для того, чтобы хранить значения WHERE
+			foreach(KeyValuePair<string,object> col in columns)
+			{
+				conditions += $"[{col.Key}] = @{col.Key} AND ";
+			}
+
+			conditions = conditions.Substring(0,conditions.Length-5);//здесь убираем повторение в виде AND
+
+			string cmd = $"SELECT COUNT(*) FROM {table} WHERE {conditions}";//считаем количество строк, где выполняются условия conditions
+			SqlCommand command = new SqlCommand (cmd);//создаем объект комманды для выполнения запроса
+			foreach(KeyValuePair<string,object> col in columns)//проходим по словарю и добавляем значения параметров в команду
+			{
+				command.Parameters.AddWithValue($"@{col.Key}", col.Value);
+
+			}
+			int count = (int)Scalar(command);//преобразуем object в int
+			return count > 0;//если такая запись в таблице есть, то мы возвращаем true count>0, а если нет, то false count=0
+		}
+
+		//Метод вставки значений в таблицу базы данных
+		public void Insert(string table, Dictionary<string,object>unique_columns,Dictionary<string,object>all_columns)
+		{
+			string pk = GetPrimaryKeyName(table);//получаем название перивчного ключа
+			if (pk != null && !all_columns.ContainsKey(pk))//проверяем существует ли он вообще и был ли передан в словаре
+			{
+				int next_id = GetNextPrimaryKey(table);//если этого не было, то мы ищем следующий id с помощью метода GetNextPrimaryKey
+				all_columns.Add(pk, next_id);//добавляем в словарь ключ/значение
+			}
+
+			if(RecordExists(table,unique_columns))//проверяем существует ли запись в таблице, если есть, то ничего не добавляем
+			{
+                Console.WriteLine("Запись с такими параметрами уже существует!");
+				return;
+			}
+
+			string fields = "";
+			string values = "";
+
+			bool is_first = true;	//флаг для постановки запятой, если первая итерация то запятая не ставится
+			foreach(KeyValuePair<string,object> col in all_columns)
+			{
+				if(!is_first)
+				{
+					fields += ", ";
+					values += ", ";
+				}
+				fields += "[" + col.Key + "]";
+				values += "@" + col.Key;
+				is_first = false;
+			}
+
+			string cmd = "INSERT INTO " + table + " (" + fields + ") VALUES (" +values +");";
+			SqlCommand command = new SqlCommand (cmd,connection);
+
+			foreach(KeyValuePair<string,object> col in all_columns)
+			{
+				command.Parameters.AddWithValue("@" + col.Key, col.Value);
+			}
+			connection.Open ();
+			try
+			{
+				command.ExecuteNonQuery ();
+				Console.WriteLine("Запись успешно добавлена");
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+			}
+			connection.Close ();
+		}
 	}
 }
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -31,13 +31,69 @@ namespace ADO
 				"director = director_id"
 				);
 			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
-			
+
 			// Меняем команду на выбор количества записей в таблице Movies
 			//command.CommandText = "SELECT COUNT(*) FROM Movies";
 			// Выполняем новый запрос и получаем единственное скалярное значение — количество записей
 			//Console.WriteLine($"Количество записей: \t{command.ExecuteScalar()}");
 			// Закрываем соединение с базой данных
 			//connection.Close();
+			
+
+
+			//string pk = connector.GetPrimaryKeyName("Directors");
+			//string pk2 = connector.GetPrimaryKeyName("Movies");
+			//Console.WriteLine($"Имя первичного ключа таблицы {pk}");
+			//Console.WriteLine($"Имя первичного ключа таблицы {pk2}");
+
+			//int number_pk = connector.GetNextPrimaryKey("Directors");
+			//Console.WriteLine($"Следующий ПК {number_pk}");
+
+			//var director = new Dictionary<string, object>
+			//{
+			//	{"first_name","James" },
+			//	{"last_name","Cameron" }
+			//};
+
+			//bool exists = connector.RecordExists("Directors",director);
+			//if (exists)
+			//{
+			//	Console.WriteLine("Запись найдена в базе данных");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Запись не найдена в базе данных");
+			//}
+
+			//Dictionary<string,object> unique = new Dictionary<string,object>();
+			//unique.Add("first_name","James");
+			//unique.Add("last_name","Cameron");
+			//Dictionary<string,object> all = new Dictionary<string,object>();
+			//all.Add("first_name","");
+			//all.Add("last_name","");
+
+			//Dictionary<string, object> unique = new Dictionary<string, object>();
+			//unique.Add("first_name", "Леонид");
+			//unique.Add("last_name", "Гайдай");
+			//Dictionary<string, object> all = new Dictionary<string, object>();
+			//all.Add("first_name", "Леонид");
+			//all.Add("last_name", "Гайдай");
+			//connector.Insert("Directors",unique,all);
+
+			connector.Select("*", "Directors");
+
+			//Dictionary<string, object> unique = new Dictionary<string, object>();
+			//unique.Add("title", "Иван Васильевич меняет профессию");
+			//unique.Add("release_date", "1973-09-17");
+			//unique.Add("director","13");
+			//Dictionary<string, object> all = new Dictionary<string, object>();
+			//all.Add("title", "Иван Васильевич меняет профессию");
+			//all.Add("release_date", "1973-09-17");
+			//all.Add("director", "13");
+			//connector.Insert("Movies", unique, all);
+
+			connector.Select("*", "Movies");
+
 		}
 		
 	}
