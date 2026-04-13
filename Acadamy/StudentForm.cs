a79d0cef0b70a73c951b1b1b2df4dedb6638682a
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-04-14 01:19:03 +0800

    Add new Student with Photo

--- Acadamy/StudentForm.cs
+++ Acadamy/StudentForm.cs
@@ -46,9 +46,20 @@ namespace Acadamy
 			base.buttonOk_Click(sender, e);
 			student = new Models.Student(human,Convert.ToInt32(cbGroup.SelectedValue));
 			//object id = (int)DataBase.Connector.Scalar($"SELECT stud_id FROM Students WHERE {student.GetCondition()}");
-			if(student.id==0)DataBase.Connector.Insert("Students", $"{student.GetNames()}",$"{student.GetValues()}");
+			if(student.id==0)student.id =Convert.ToInt32(DataBase.Connector.Scalar
+				(
+				$"INSERT Students({student.GetNames()}) VALUES ({student.GetValues()});SELECT SCOPE_IDENTITY()")
+				);
 			else DataBase.Connector.Update($"UPDATE Students SET {student.GetUpdateString()} WHERE stud_id={student.id}");
-			if(student.photo!= null)DataBase.Connector.UploadPhoto(student.SerializePhoto(),student.id,"photo","Students");
+			if (student.photo != null)
+			{
+				//if (student.id == 0)
+				//	student.id = Convert.ToInt32(DataBase.Connector.Scalar
+				//	(
+				//	"$SELECT stud_id FROM Students WHERE {student.GetCondition()}"
+				//	);
+				DataBase.Connector.UploadPhoto(student.SerializePhoto(), student.id, "photo", "Students");
+			}
 			//try
 			//{
 			//	DataBase.Connector.Insert
